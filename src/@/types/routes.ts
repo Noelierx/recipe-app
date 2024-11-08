0108ec459ad89d7b603e76c9bf2d7115866b34b8
Noëlie <noelieroux11@gmail.com>
2024-11-08 14:46:03 +0100

    :bug: send complete files

---
+++ src/@/types/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+    HOME: '/',
+    ADD_RECIPE: '/add-recipe',
+    EDIT_RECIPE: '/edit-recipe',
+    RECIPE_DETAILS: '/recipe'
+  } as const;
---
+++ src/@/types/translations.ts
@@ -0,0 +1,6 @@
+export const TITLES = {
+    recipe_list: 'Liste des recettes',
+    recipe_details: 'Détails de la recette',
+    add_recipe: 'Ajouter une recette',
+    edit_recipe: 'Modifier la recette'
+  } as const;
