8990632589310257821035008078999ebbc67bfa
Noëlie <noelieroux11@gmail.com>
2025-02-16 11:14:59 +0100

    🚧 test routing

--- src/@/types/routes.ts
+++ src/@/types/routes.ts
@@ -1,11 +1,7 @@
-type RouteParams = {
-  id: string;
-};
-
 export const ROUTES = {
-    HOME: '/',
-    ADD_RECIPE: '/add-recipe',
-    RECIPE_DETAILS: (params: Pick<RouteParams, 'id'>) => `/recipe/${params.id}`,
-    EDIT_RECIPE: (params: Pick<RouteParams, 'id'>) => `/recipe/${params.id}/edit`,
-    MEAL_PLANNER: '/meal-planner'
-  } as const;
+  HOME: '/',
+  RECIPE_DETAILS: ({ id }: { id: string }) => `/recipe/${id}`,
+  ADD_RECIPE: '/add-recipe',
+  EDIT_RECIPE: ({ id }: { id: string }) => `/edit-recipe/${id}`,
+  MEAL_PLANNER: '/meal-planner',
+};
