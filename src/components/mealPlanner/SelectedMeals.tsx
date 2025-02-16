b35c8571f2488c41e3c600dfa1ab591dc469da91
Noëlie <noelieroux11@gmail.com>
2025-02-16 14:54:07 +0100

    ✨ make title of recipe clickable

--- src/components/mealPlanner/SelectedMeals.tsx
+++ src/components/mealPlanner/SelectedMeals.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { WeeklyPlan } from '@/types/mealPlannerTypes';
 import { RecipeWithDetails } from '@/types/RecipeTypes';
 
@@ -8,7 +9,7 @@ interface SelectedMealsProps {
 }
 
 const SelectedMeals: React.FC<SelectedMealsProps> = ({ weeklyPlan, recipes }) => {
-    const mealServingsMap: Record<string, number> = {};
+    const mealServingsMap: Record<string, { servings: number, recipeId: number }> = {};
 
     Object.values(weeklyPlan).forEach(dayPlan => {
         Object.values(dayPlan).forEach(meal => {
@@ -17,9 +18,9 @@ const SelectedMeals: React.FC<SelectedMealsProps> = ({ weeklyPlan, recipes }) =>
                 if (recipe) {
                     const key = recipe.title;
                     if (!mealServingsMap[key]) {
-                        mealServingsMap[key] = 0;
+                        mealServingsMap[key] = { servings: 0, recipeId: recipe.id };
                     }
-                    mealServingsMap[key] += meal.servings;
+                    mealServingsMap[key].servings += meal.servings;
                 }
             }
         });
@@ -29,10 +30,12 @@ const SelectedMeals: React.FC<SelectedMealsProps> = ({ weeklyPlan, recipes }) =>
         <div className="border rounded-lg m-4 p-4 max-w-md">
             <h2 className="text-2xl font-bold mb-4">Repas sélectionnés</h2>
             <ul className="space-y-2">
-                {Object.entries(mealServingsMap).map(([mealName, totalServings]) => (
+                {Object.entries(mealServingsMap).map(([mealName, { servings, recipeId }]) => (
                     <li key={mealName} className="flex justify-between items-center">
-                        <span>{mealName}</span>
-                        <span className="ml-auto">{totalServings} portions</span>
+                        <Link to={`/recipe/${recipeId}`}>
+                            {mealName}
+                        </Link>
+                        <span className="ml-auto">{servings} portions</span>
                     </li>
                 ))}
             </ul>
